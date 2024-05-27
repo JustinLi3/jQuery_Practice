505c17110fe7e265d7d7b079b9b03e949aaa26f3
JustinLi3 <Triamethyest@gmail.com>
2024-05-27 12:06:45 -0400

    Saving

--- Resources/jQuery Starter/script.js
+++ Resources/jQuery Starter/script.js
@@ -1,1 +1,5 @@
-console.log("Welcome to The Ultimate jQuery Course!🚀");
+console.log("Welcome to The Ultimate jQuery Course!🚀"); 
+
+// $('li').hide(); //Essentially the same notation for grabbing elements in querySelector  
+$("li .Second").hide();
+
