f36c641c74d4878317ff9843f721dfd06f7c95ce
JustinLi3 <Triamethyest@gmail.com>
2024-05-28 22:46:38 -0400

    JQuery events and the hide/show function

--- Resources/jQuery Starter/script.js
+++ Resources/jQuery Starter/script.js
@@ -1,5 +1,54 @@
 console.log("Welcome to The Ultimate jQuery Course!🚀"); 
+ 
+
+//SELECTORS 
 
 // $('li').hide(); //Essentially the same notation for grabbing elements in querySelector  
-$("li .Second").hide();
+// $("li.Second").hide(); //tag.classname .classname etc etc to grab specific items
+// $("p#heading").hide(); //This is just way better than using document.getElementByClassName or by id  
 
+//Check documentation for other kinds of selectors like multiple elements  
+
+
+//EVENTS 
+// Mouse / Keyboard / Form / Document or Window Events
+
+//Mouse: .click() serves as .addEventListener()
+$("#btn_hide").click(function(){
+    $("li.Second").hide();
+}); 
+ 
+//keyboard  keydown:held down any key  keypress:held down any character  keyup:event that happens after key lifted up
+$("*").keydown(function(event){ 
+    if(event.key ==='h'){ //use event.which or event.key 
+        $("li.Second").hide();
+    } 
+    else if(event.key ==='s'){
+        $("li.Second").show();
+    }
+}) 
+
+
+
+//HIDE & SHOW & CREATE FUNCTIONS  
+//.hide/show(x miliseconds, function()); 
+//ex:   
+
+
+$("*").keyup(function(event){
+    if(event.key === 'Enter'){
+        $("*").css("background-color", "lightblue"); //.css to change a css factor
+    }
+});
+
+$('#funnyButton').click(function(){
+    $('button#btn_hide').hide(2000, function(){
+        $("*").css("height","2000px");
+    });
+});
+
+
+//NOTE, make sure you include .ready function for the website to be loaded before proceeding to use javascript/jquery
+$('document').ready(function(){
+//Include all js in here
+});
